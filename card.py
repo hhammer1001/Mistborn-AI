89c53ee9fa2c5b202d1d14e48d8b426842d8ae6a
Eli-Pregerson <epregerson@hmc.edu>
2025-01-24 21:40:40 -0800

    refactoring Card

---
+++ card.py
@@ -0,0 +1,34 @@
+class Card():
+
+    def __init__(self, name, data, deck):
+        #data = [Cost,metal code,ability 1,abil 1 amt,ability 2,abil 2 amt,ability 3,abil 3 amt,activ abil,activ amt,burn abil,burn amt]
+        self.name = name
+        self.deck = deck
+        self.cost = data[0]
+        self.metal = data[1]
+        self.ability1 = [zip(data[2].split("."), data[3].split("."))]
+        # self.ability1
+
+
+    def play(self, owner):
+        for func, arg in self.ability1:
+            self.deck.owner.missionFuncs[func](int(arg))
+        self.deck.inPlay += [self]
+        #TODO
+
+    def __repr__(self):
+        return self.name
+
+class Action(Card):
+    def __init__(self, name, data, deck):
+        #data = [Cost,metal code,ability 1,abil 1 amt,ability 2,abil 2 amt,ability 3,abil 3 amt,activ abil,activ amt,burn abil,burn amt]
+        self.name = name
+        self.deck = deck
+        self.cost = data[0]
+        self.metal = data[1]
+        self.ability1 = [zip(data[2].split("."), data[3].split("."))]
+        # self.ability1
+
+class Ally(Card):
+
+class Funding(Card):
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import random
 import csv
+import deck
 
 """ Mission tier format [dist from last reward/start, reward func, amt, first player reward func, first player reward amt]"""
 
@@ -78,27 +79,6 @@ class Mission():
         self.progress += amount
         # if self.progress > self.distToNextTier:
         #     self.progress = self.progess - self.
-
-class Card():
-
-    def __init__(self, name, data, deck):
-        #data = [Cost,metal code,ability 1,abil 1 amt,ability 2,abil 2 amt,ability 3,abil 3 amt,activ abil,activ amt,burn abil,burn amt]
-        self.name = name
-        self.deck = deck
-        self.cost = data[0]
-        self.metal = data[1]
-        self.ability1 = [zip(data[2].split("."), data[3].split("."))]
-        # self.ability1
-
-
-    def play(self, owner):
-        for func, arg in self.ability1:
-            self.deck.owner.missionFuncs[func](int(arg))
-        self.deck.inPlay += [self]
-        #TODO
-
-    def __repr__(self):
-        return self.name
         
 class Player():
 
