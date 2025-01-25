b85feeb0aea6ee6e4f10645a65f22e7480ebf16e
Eli-Pregerson <epregerson@hmc.edu>
2025-01-24 21:07:11 -0800

    refactoring deck

--- deck.py
+++ deck.py
@@ -1,35 +1,12 @@
+import random
+
 class Deck():
 
     def __init__(self, gameName):
         self.game = gameName
-
-    def flip(self):
-        for x in range(6-len(self.game.market)):
-            newCard = self.draw(1)[0]
-            self.game.market += [newCard]
-            print(f"{newCard} added to market")
-    
-
-
-class PlayerDeck(Deck):
-
-    def __init__(self, gameName, code):
-        super().__init__(gameName)
         self.hand = []
         self.cards = []
-        if code in ['Kelsier', 'Shan']:
-            for c in deckInfo[0]:
-                data = cardLookup[c]
-                self.cards += [Card(c, data, self)]
-        elif code in ['Vin', 'Marsh', 'Prodigy']:
-            pass
-        
         self.discard = []
-        self.inPlay = []
-    
-    def __repr__(self):
-        out = self.cards + self.discard + self.hand + self.inPlay
-        return str(out)
     
     def draw(self, amount):
         for i in range(amount):
@@ -39,6 +16,32 @@ class PlayerDeck(Deck):
                 if self.cards == []:
                     pass
             self.hand += [self.cards.pop(0)]
+
+
+    
+
+
+class PlayerDeck(Deck):
+
+    def __init__(self, gameName, code):
+        super().__init__(gameName)
+
+        if code in ['Kelsier', 'Shan']:
+            for c in deckInfo[0]:
+                data = cardLookup[c]
+                self.cards += [Card(c, data, self)]
+        elif code in ['Vin', 'Marsh', 'Prodigy']:
+            pass
+        random.shuffle(self.cards)
+        
+        
+        self.inPlay = []
+    
+    def __repr__(self):
+        out = self.cards + self.discard + self.hand + self.inPlay
+        return str(out)
+    
+    
     
     def discardHand(self):
         self.discard += self.hand
@@ -54,3 +57,17 @@ class PlayerDeck(Deck):
                 self.deck.inPlay = self.deck.inPlay[:choice-h] + self.deck.inPlay[choice-h+1:]
             else:
                 self.deck.discard = self.deck.discard[:choice-h-p] + self.deck.discard[choice-h-p+1:]
+    
+    def add(self, card):
+        self.discard += [card]
+
+class Market(Deck):
+    def __init__(self, gameName):
+        super().__init__(gameName)
+        #TODO self.cards = 
+        random.shuffle(self.cards)
+        self.draw(6)  
+    
+    def remove(self, choice):
+        del self.cards[choice]
+        self.draw(1)
