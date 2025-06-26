3e5de3255552f0b79470aaab1ac229dcf7ea67a5
Eli-Pregerson <epregerson@hmc.edu>
2025-06-26 14:14:32 -0700

    final commit before I go dark

--- battle.py
+++ battle.py
@@ -2,10 +2,15 @@ from game import Game
 from robot import RandomBot
 
 def main():
+    dic = {"Kaladin": 0,
+    "Jasnah": 0}
     for i in range(10):
         g = Game(randos = True)
         winner = g.play()
+        dic[winner.name] += 1
+        
         print(f"The winner is {winner}!!!!!")
+    print(dic)
 
 if __name__ == '__main__':
     main()
--- robot.py
+++ robot.py
@@ -86,3 +86,87 @@ class RandomBot(Player):
     def cloudP(self, card):
         #return a bool to decide if the card should be discarded to cloudp
         return random.random() > 0.5
+
+class EliBot(Player):
+    def assignDamageIn(self, targets):
+        #choose the index of the ally to kill by assigning damage to it or return -1 to go to player damage
+        return random.randint(0, len(targets)) - 1
+
+    def selectAction(self, actions, game):
+        #choose and return one of the actions in the list
+        return random.choice(actions)
+    
+    def senseCheckIn(self, card):
+        #return a bool to use the sense ability
+        return random.random() > 0.5
+
+    def killEnemyAllyIn(self, allies):
+        #return the index of the ally to kill or -1 to not kill any
+        return random.randint(0, len(allies)) - 1
+    
+    def cloudAlly(self, card, ally):
+        #return a bool to discard card to save ally 
+        return random.random() > 0.5
+    
+    def eliminateIn(self):
+        #return the index of the card to be eliminated or -1 to not eliminate. Indexing goes hand, discard
+        h = len(self.deck.hand)
+        d = len(self.deck.discard)
+        return random.randint(0, h + d) - 1
+    
+    def pullIn(self):
+        #return the index of the card to be pulled or -1 to not pull.
+        return random.randint(0,len(self.deck.discard)) - 1
+        
+    
+    def subdueIn(self, choices):
+        #return the index of the card to gain. All are options are valid targets -1 to pick nothing
+        return random.randint(0,len(choices)) - 1
+    
+    def soarIn(self, choices):
+        #return the index of the eliminated card to buy or -1 to not
+        return random.randint(0,len(choices)) - 1
+
+    def confrontationIn(self, choices):
+        #return the index of the card to use it's first ability or -1 to not
+        return random.randint(0,len(choices)) - 1
+    
+    def informantIn(self, card):
+        #return a bool to decide if card is removed from top of deck
+        return random.random() > 0.5
+
+    def keeperIn(self, choices):
+        #return the index of the card in play to be set aside
+        return random.randint(0,len(choices)) - 1
+    
+    def chooseIn(self, options):
+        #each effect takes two indices. for the first effect return 0, and for the second return 2 etc.
+        k = (len(options) // 2) - 1
+        return random.randint(0, k)
+    
+    def refreshIn(self):
+        #return the index of the metal to refresh
+        return random.randint(0,7)
+
+    def pushIn(self):
+        #return the index of the market card to push
+        return random.randint(-1, 5)
+    
+    def riotIn(self, riotable):
+        #choose the ally from the list of valid allies to riot
+        return random.choice(riotable)
+    
+    def seekIn(self, twice, seeker, choices):
+        #im not going to try to explain this one... Henry I blame you for what's going on here
+        choice = random.randint(0, len(choices) - 1)
+        if twice:
+            choice2 = random.randint(0, len(choices) - 2)
+        else:
+            choice2 = -1
+        if choice == choice2:
+            choice2 += 1
+        return choice, choice2
+    
+    def cloudP(self, card):
+        #return a bool to decide if the card should be discarded to cloudp
+        return random.random() > 0.5
